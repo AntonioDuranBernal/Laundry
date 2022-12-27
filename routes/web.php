463a4a4ec7bd5656939eda508ed6337c7ee1e762
Antonio Bernal <duranbernalla@gmail.com>
2022-12-27 09:44:54 -0600

    funcion bien, por agregar clientes y gestionarlos

--- routes/web.php
+++ routes/web.php
@@ -6,26 +6,25 @@ use App\Http\Controllers\ClientesController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\AutheticatedSessionController;
 
+Route::get('inicio',[NotaController::class, 'inicio'])->name('inicio');
+Route::get('/',[NotaController::class, 'welcome'])->name('welcome');
+Route::view('/registro','auth.registro')->name('registro');
+
 Route::post('/ustore',[RegisteredUserController::class, 'ustore'])->name('user.store');
 Route::post('/sesion',[AutheticatedSessionController::class, 'astore'])->name('session');
 Route::post('/terminar',[AutheticatedSessionController::class, 'destroy'])->name('logout');
-Route::view('/registro','auth.registro')->name('registro');
 Route::get('/login',[RegisteredUserController::class, 'login'])->name('login');
 
-Route::view('/clientes','clientes.inicioClientes')->name('clientes.inicioClientes');
-Route::view('/notas','notas.inicioNotas')->name('notas.inicioNotas');
 Route::view('/prendas','prendas.inicioPrendas')->name('prendas.inicioPrendas');
 Route::view('/servicios','servicios.inicioServicios')->name('servicios.inicioServicios');
 
-Route::get('inicio',[NotaController::class, 'inicio'])->name('inicio');
-Route::get('/',[NotaController::class, 'welcome'])->name('welcome');
-
+Route::view('/notas','notas.inicioNotas')->name('notas.inicioNotas');
 Route::get('/nuevanota',[NotaController::class, 'nuevanota'])->name('notas.nuevanota');
 Route::post('/buscar',[NotaController::class, 'search'])->name('notas.search');
 Route::get('/listado',[NotaController::class, 'index'])->name('notas.index');
 Route::patch('/creando',[NotaController::class, 'store'])->name('notas.store');
 Route::post('/datosEntrega',[NotaController::class, 'storeDatosCliente'])->name('notas.storeDatosCliente');
-Route::get('/registro/{idr}',[NotaController::class, 'indexdetallenotas'])->name('notas.indexdetallenotas');
+Route::get('/listado/{idr}',[NotaController::class, 'indexdetallenotas'])->name('notas.indexdetallenotas');
 Route::post('/guardardn',[NotaController::class, 'storeDetallesNota'])->name('notas.storeDetallesNota');
 Route::post('/addDetalle/{idr}',[NotaController::class, 'addDetalle'])->name('notas.addDetalle');
 Route::delete('/cancelar/{idNota}',[NotaController::class, 'cancelarNota'])->name('notas.cancelarNota');
@@ -40,9 +39,10 @@ Route::get('/mostrar/{id}',[NotaController::class, 'show'])->name('notas.show');
 Route::patch('/entregaMenu',[NotaController::class, 'datosEntregaMenu'])->name('notas.datosEntregaMenu');
 Route::patch('/actualizarEntrega',[NotaController::class, 'updateCreate'])->name('notas.updateCreate');
 
+Route::view('/clientes','clientes.inicioClientes')->name('clientes.inicioClientes');
 Route::post('/buscarCliente',[ClientesController::class, 'search'])->name('clientes.search');
 Route::delete('/eliminar/{idc}',[ClientesController::class, 'eliminarCliente'])->name('clientes.eliminarCliente');
-Route::view('/registro','clientes.addCliente')->name('clientes.nuevo');
+Route::view('/registrarcliente','clientes.addCliente')->name('clientes.nuevo');
 Route::post('/agregarCliente',[ClientesController::class, 'agregar'])->name('clientes.agregar');
 
 
